97f77e5434b95b1d629eebcda7905131dcd9192f
bgupta-commits <bgupta@charlotte.edu>
2026-04-15 18:49:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ This app explores the historical data from Dr. Ignaz Semmelweis, who discovered 
 **handwashing** significantly reduced mortality rates in maternity wards in the 1840s.
 """)
 
+
+# Replace 'your_file_name.csv' with the actual path to your CSV file
+df = pd.read_csv('/yearly_deaths_by_clinic-1.csv')
+
+display(df)
